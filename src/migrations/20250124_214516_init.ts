ca60b06d1ccbcab190972346f1aa8adb7bbb5fc6
Dmitriy Gerasimov <up@websector.ru>
2025-01-25 00:51:47 +0300

    feat: миграции бд

---
+++ src/migrations/20250124_214516_init.ts
@@ -0,0 +1,116 @@
+import { MigrateUpArgs, MigrateDownArgs } from '@payloadcms/db-postgres'
+import { sql } from 'drizzle-orm'
+
+export async function up({ payload }: MigrateUpArgs): Promise<void> {
+await payload.db.drizzle.execute(sql`
+
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+	"email" varchar NOT NULL,
+	"reset_password_token" varchar,
+	"reset_password_expiration" timestamp(3) with time zone,
+	"salt" varchar,
+	"hash" varchar,
+	"login_attempts" numeric,
+	"lock_until" timestamp(3) with time zone
+);
+
+CREATE TABLE IF NOT EXISTS "subscriptions_links" (
+	"_order" integer NOT NULL,
+	"_parent_id" integer NOT NULL,
+	"id" varchar PRIMARY KEY NOT NULL,
+	"url" varchar NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "subscriptions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar NOT NULL,
+	"slug" varchar NOT NULL,
+	"subscription_url" varchar,
+	"expire" timestamp(3) with time zone NOT NULL,
+	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "payload_preferences" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"key" varchar,
+	"value" jsonb,
+	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "payload_preferences_rels" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"order" integer,
+	"parent_id" integer NOT NULL,
+	"path" varchar NOT NULL,
+	"users_id" integer
+);
+
+CREATE TABLE IF NOT EXISTS "payload_migrations" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar,
+	"batch" numeric,
+	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "shared_config" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"vpn_name" varchar NOT NULL,
+	"config_update_hours" numeric NOT NULL,
+	"support_chat_link" varchar NOT NULL,
+	"site_link" varchar NOT NULL,
+	"announce" varchar NOT NULL,
+	"updated_at" timestamp(3) with time zone,
+	"created_at" timestamp(3) with time zone
+);
+
+CREATE INDEX IF NOT EXISTS "users_created_at_idx" ON "users" ("created_at");
+CREATE UNIQUE INDEX IF NOT EXISTS "users_email_idx" ON "users" ("email");
+CREATE INDEX IF NOT EXISTS "subscriptions_links_order_idx" ON "subscriptions_links" ("_order");
+CREATE INDEX IF NOT EXISTS "subscriptions_links_parent_id_idx" ON "subscriptions_links" ("_parent_id");
+CREATE UNIQUE INDEX IF NOT EXISTS "subscriptions_slug_idx" ON "subscriptions" ("slug");
+CREATE INDEX IF NOT EXISTS "subscriptions_created_at_idx" ON "subscriptions" ("created_at");
+CREATE INDEX IF NOT EXISTS "payload_preferences_key_idx" ON "payload_preferences" ("key");
+CREATE INDEX IF NOT EXISTS "payload_preferences_created_at_idx" ON "payload_preferences" ("created_at");
+CREATE INDEX IF NOT EXISTS "payload_preferences_rels_order_idx" ON "payload_preferences_rels" ("order");
+CREATE INDEX IF NOT EXISTS "payload_preferences_rels_parent_idx" ON "payload_preferences_rels" ("parent_id");
+CREATE INDEX IF NOT EXISTS "payload_preferences_rels_path_idx" ON "payload_preferences_rels" ("path");
+CREATE INDEX IF NOT EXISTS "payload_migrations_created_at_idx" ON "payload_migrations" ("created_at");
+DO $$ BEGIN
+ ALTER TABLE "subscriptions_links" ADD CONSTRAINT "subscriptions_links_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "subscriptions"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "payload_preferences"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+`);
+
+};
+
+export async function down({ payload }: MigrateDownArgs): Promise<void> {
+await payload.db.drizzle.execute(sql`
+
+DROP TABLE "users";
+DROP TABLE "subscriptions_links";
+DROP TABLE "subscriptions";
+DROP TABLE "payload_preferences";
+DROP TABLE "payload_preferences_rels";
+DROP TABLE "payload_migrations";
+DROP TABLE "shared_config";`);
+
+};
