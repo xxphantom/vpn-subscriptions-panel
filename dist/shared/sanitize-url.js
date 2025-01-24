514b1338363b9c316066fa47a1ba5d8ef6757659
Dmitriy Gerasimov <up@websector.ru>
2025-01-24 14:12:51 +0300

    feat: toLowerCase ник в url подписки

--- dist/shared/sanitize-url.js
+++ dist/shared/sanitize-url.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sanitizeUrl = void 0;
 function sanitizeUrl(str) {
     var replacedSpaces = str.replace(/\s+/g, "-");
-    return replacedSpaces.replace(/[^a-zA-Z0-9-_]/g, "");
+    return replacedSpaces.replace(/[^a-zA-Z0-9-_]/g, "").toLowerCase();
 }
 exports.sanitizeUrl = sanitizeUrl;
